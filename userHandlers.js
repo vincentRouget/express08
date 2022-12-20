37d54ecaafa13b851fa7746915e10b41499e0b50
vincentRouget <vincent.rouget86@gmail.com>
2022-12-20 09:09:34 +0100

    express08

---
+++ userHandlers.js
@@ -0,0 +1,139 @@
+const { UNSAFE_NavigationContext } = require("react-router-dom");
+const database = require("./database");
+
+const getUsers = (req, res) => {
+    const initialSql = "select * from users";
+    const where = [];
+
+    if (req.query.language != null) {
+        where.push({
+            column: "language",
+            value: req.query.language,
+            operator: "=",
+        });
+    }
+    if (req.query.city != null) {
+        where.push({
+            column: "city",
+            value: req.query.city,
+            operator: "=",
+        });
+    }
+
+    database
+        .query(
+            where.reduce(
+                (sql, { column, operator }, index) =>
+                    `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+                initialSql
+            ),
+            where.map(({ value }) => value)
+        )
+        .then(([movies]) => {
+            res.json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error retrieving data from database");
+        });
+};
+
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query("select * from users where id = ?", [id])
+        .then(([users]) => {
+            if (users[0] != null) {
+                res.status(200).json(users[0]);
+            } else {
+                res.status(404).send("Not Found");
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error retrieving data from database");
+        });
+};
+
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+    const { email } = req.body;
+
+    database
+        .query("select * from users where email = ?", [email])
+        .then(([users]) => {
+            if (users[0] != null) {
+                req.user = users[0];
+                next();
+            } else {
+                res.sendStatus(401);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error retrieving data from database");
+        });
+};
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language, hashedPassword } = req.body;
+
+    database
+        .query(
+            "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+            [firstname, lastname, email, city, language, hashedPassword]
+        )
+        .then(([result]) => {
+            res.location(`/api/users/${result.insertId}`).sendStatus(201);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error saving the user");
+        });
+};
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { firstname, lastname, email, city, language } = req.body;
+    database
+        .query(
+            "UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ?, language = ? WHERE id = ?",
+            [firstname, lastname, email, city, language, id]
+        )
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.status(204).send("User modified");
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error editing the user");
+        });
+};
+
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query("delete from users where id = ?", [id])
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error deleting the user");
+        });
+};
+
+module.exports = {
+    getUsers,
+    getUserById,
+    postUser,
+    updateUser,
+    deleteUser,
+    getUserByEmailWithPasswordAndPassToNext,
+};
